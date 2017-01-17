31e0c3559cc76537106d0e5efee6c626d3d993fd
Kevin Jang <pianistkevinjang@gmail.com>
2017-01-16 16:17:00 -0800

    Create User model

--- app/models/user.js
+++ app/models/user.js
@@ -4,8 +4,26 @@ var Promise = require('bluebird');
 
 
 var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
 
+  initialize: function() {
+    this.on('creating', function(model, attrs, options) {
+      // Get pass attribute from model
+      // Hash pass
+      // Set password to hash
+      var salt = bcrypt.genSaltSync();
+      var pass = bcrypt.hash(model.get('password'), salt, function(err, results) {
+        if (err) {
+          throw err;
+        } else {
+          model.set('password', results);
+        }
+      });
+    });
+  }
 });
+
 
 
 module.exports = User;
